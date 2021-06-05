ab7564e5964085ef3c0487424e550df64c0c60ca
Carlos Oliveira <oslcarlos@gmail.com>
2021-06-04 23:32:20 -0300

    Primeiro Projeto

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo primeiro projeto");
+            Console.WriteLine("Precione o enter para finalizar o programa");
+            Console.ReadLine();
+        }
+    }
+}
